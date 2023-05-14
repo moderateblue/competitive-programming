083a1a87216eac690c28258df55ff67466942074
moderateblue <127915442+moderateblue@users.noreply.github.com>
2023-05-13 17:13:49 -0700

    Create <6 kyu> Traffic Lights - one car (5d0ae91acac0a50232e8a547).java

---
+++ codewars/<6 kyu> Traffic Lights - one car (5d0ae91acac0a50232e8a547).java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+
+public class Dinglemouse {
+  public static String[] trafficLights(String road, int n) {
+    char[] currRoad = road.toCharArray();
+    int[] lightTimer = new int[road.length()];
+    Arrays.fill(lightTimer, -1);
+    
+    //10-6 G
+    //5 O
+    //4-0 R
+    for (int i = 0; i < currRoad.length; i++) {
+      if (currRoad[i] == 'R') lightTimer[i] = 4;
+      else if (currRoad[i] == 'G') lightTimer[i] = 10;
+   }
+    
+    String[] roads = new String[n + 1];
+    roads[0] = road;
+    
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < lightTimer.length; j++) {
+        if (lightTimer[j] == 0) {
+          lightTimer[j] = 10;
+          if (currRoad[j] != 'C') currRoad[j] = 'G';
+        }
+        else if (lightTimer[j] != -1) {
+          lightTimer[j]--;
+          if (lightTimer[j] == 5) {
+            if (currRoad[j] != 'C') currRoad[j] = 'O';
+          }
+          else if (lightTimer[j] <= 4) {
+            if (currRoad[j] != 'C') currRoad[j] = 'R';
+          }
+          else if (lightTimer[j] >= 6) {
+            if (currRoad[j] != 'C') currRoad[j] = 'G';
+          }
+        }
+      }
+      for (int j = 0; j < lightTimer.length; j++) {
+        if (currRoad[j] == 'C') {
+          if (j == lightTimer.length - 1 && lightTimer[j] == -1) {
+            currRoad[j] = '.';
+          }
+          else if (j == lightTimer.length - 1 && lightTimer[j] != -1) {
+            if (lightTimer[j] == 5) currRoad[j] = 'O';
+            else if (lightTimer[j] <= 4) currRoad[j] = 'R';
+            else if (lightTimer[j] >= 6) currRoad[j] = 'G';
+          }
+          else if (currRoad[j + 1] == '.' && lightTimer[j] == -1) {
+            currRoad[j] = '.';
+            currRoad[j + 1] = 'C';
+          }
+          else if (currRoad[j + 1] == '.' && lightTimer[j] != -1) {
+            if (lightTimer[j] == 5) currRoad[j] = 'O';
+            else if (lightTimer[j] <= 4) currRoad[j] = 'R';
+            else if (lightTimer[j] >= 6) currRoad[j] = 'G';
+            currRoad[j + 1] = 'C';
+          }
+          else if (currRoad[j + 1] == 'G') {
+            currRoad[j] = '.';
+            currRoad[j + 1] = 'C';
+          }
+          break;
+        }
+      }
+      roads[i] = String.valueOf(currRoad);
+    }
+    
+    return roads;
+  }
+}
